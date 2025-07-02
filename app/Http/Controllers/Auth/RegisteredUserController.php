d5e68bb53cf2e7e8b42b4e7b9b5e35415c500578
Abdul Arham <arham.dstech@gmail.com>
2025-07-02 17:56:39 +0500

    Update path of default avatar

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'avatar' => '/user/avatar.png'
+            'avatar' => '/img/avatar.png'
         ]);
 
         event(new Registered($user));
